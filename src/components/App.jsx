ea7c438e4371872306963dd20bb06fc3e5b02e65
Kelvin-Ben <kevoben14@gmail.com>
2023-06-20 17:30:51 +0300

    Import a component and added styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SongList from "./SongList";
-
+import SongDetails from "./SongDetails";
 const App = () => {
   return (
-    <div>
-      <SongList />
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetails />
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default App;
